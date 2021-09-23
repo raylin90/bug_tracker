2aa9d20effd96fe9dbdae93c9bb511a634ff37c9
Ray Lin <rayz.lin90@gmail.com>
2021-09-23 00:26:20 -0400

    stuck at save_user method (registration)

--- flask_app/controllers/users.py
+++ flask_app/controllers/users.py
@@ -2,6 +2,10 @@
 from flask_app import app
 # import render_template(view HTML), redirect(route to different URL), request(get data from HTML), session(store session value), flask(display flash message
 from flask import render_template,redirect,request,session, flash
+from flask_app.models.user import User
+# Bcrypt to hash the password
+from flask_bcrypt import Bcrypt
+bcrypt = Bcrypt(app)
 
 ######################################
 # dashboard route
@@ -16,3 +20,29 @@ def dashboard():
 @app.route("/register")
 def register():
     return render_template("users/register.html")
+
+@app.route("/create_user", methods = ["POST"])
+def create_user():
+    #validate if email already exist at db
+    data = { 
+        "email" : request.form["email"] 
+    }
+    user_in_db = User.get_user_by_email(data)
+    if user_in_db:
+        flash("Email already in use, please use another email!")
+        return redirect('/register')
+    #validate if the input are matching the validation rule
+    if not User.validate_user(request.form):
+        return redirect('/register')
+    # if no errors, we hash the password
+    pw_hash = bcrypt.generate_password_hash(request.form['password'])
+    # then save user info into database using prepared data dict.:
+    data = {
+        "first_name": request.form["first_name"],
+        "last_name": request.form["last_name"],
+        "email": request.form["email"],
+        "password": pw_hash,
+        "admin_id": 0,
+    }
+    User.save_user(data)
+    return redirect("/")
---
+++ flask_app/models/user.py
@@ -0,0 +1,62 @@
+# import mysqlconnection, so we can connecto SQL database
+from flask_app.config.mysqlconnection import connectToMySQL
+# import flask, so we can display flash message at HTML
+from flask import flash
+# import regex for pattern validation
+import re
+EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
+NAME_REGEX = re.compile(r'^[a-zA-Z]+$')
+
+
+# model the class after the user table from our database
+class User:
+    # match to db table columns
+    def __init__(self,data):
+        self.id = data['id']
+        self.first_name = data['first_name']
+        self.last_name = data['last_name']
+        self.email = data['email']
+        self.password = data['password']
+        self.created_at = data['created_at']
+        self.updated_at = data['updated_at']
+
+    ############################################
+    # validation registration input method
+    ############################################
+    @staticmethod
+    def validate_user(data):
+        is_valid = True
+        if not NAME_REGEX.match(data['first_name']) or len(data['first_name']) < 2:
+            flash("First name should contain letters only and at least 2 characters.")
+            is_valid = False
+        if not NAME_REGEX.match(data['last_name']) or len(data['last_name']) < 2:
+            flash("Last name should contain letters only and at least 2 characters.") 
+            is_valid = False
+        if not EMAIL_REGEX.match(data['email']): 
+            flash("Invalid email format!")
+            is_valid = False
+        if len(data['password']) < 8:
+            flash("Password should be at least 8 characters.") 
+            is_valid = False
+        if data['password'] != data['conf_password']:
+            flash("Password and Confirm Password does not match.") 
+            is_valid = False
+        return is_valid
+
+    ############################################
+    # method to check if user's email in db or not
+    ############################################
+    @classmethod
+    def get_user_by_email(cls, data):
+        query = "SELECT * FROM users WHERE email = %(email)s;"
+        result = connectToMySQL("bug_tracker").query_db(query, data)
+        if len(result) < 1:
+            return False
+        return cls(result[0])
+    ############################################
+    # method to save user's registration info.
+    ############################################
+    @classmethod
+    def save_user(cls, data):
+        query = "INSERT INTO users (first_name, last_name, email, password, admin_id, created_at) VALUES (%(first_name)s, %(last_name)s, %(email)s, %(password)s, %(admin_id)s, NOW())"
+        return connectToMySQL("bug_tracker").query_db(query, data)
